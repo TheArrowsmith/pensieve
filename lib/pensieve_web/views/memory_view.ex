8eb29eb4eb0871220431e62fa7f978b24b6273f8
George <george@arrowsmith.codes>
2022-12-19 18:27:49 +0000

    extract MemoryView.memory_inserted_at/1

--- lib/pensieve_web/views/memory_view.ex
+++ lib/pensieve_web/views/memory_view.ex
@@ -1,3 +1,9 @@
 defmodule PensieveWeb.MemoryView do
   use PensieveWeb, :view
+
+  alias Pensieve.Memories.Memory
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
+  end
 end
