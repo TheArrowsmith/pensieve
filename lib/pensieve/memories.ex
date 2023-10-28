059e2ec460c86ac10c2f61dc18a7f3e4eb4c34e4
George <george@arrowsmith.codes>
2023-10-28 19:38:15 +0100

    add typespecs to Memories context

--- lib/pensieve/memories.ex
+++ lib/pensieve/memories.ex
@@ -2,26 +2,36 @@ defmodule Pensieve.Memories do
   alias Pensieve.Repo
   alias Pensieve.Memories.Memory
 
+  @spec list_memories() :: [Memory]
   def list_memories, do: Repo.all(Memory)
 
+  @type id :: pos_integer | binary
+
+  @spec get_memory!(id) :: %Memory{}
   def get_memory!(id), do: Repo.get!(Memory, id)
 
+  @type memory_op_result :: {:ok, %Memory{}} | {:error, %Ecto.Changeset{}}
+
+  @spec change_memory(%Memory{}, map) :: %Ecto.Changeset{}
   def change_memory(%Memory{} = memory, attrs \\ %{}) do
     Memory.changeset(memory, attrs)
   end
 
+  @spec create_memory(map) :: memory_op_result
   def create_memory(attrs \\ %{}) do
     %Memory{}
     |> Memory.changeset(attrs)
     |> Repo.insert()
   end
 
+  @spec update_memory(%Memory{}, map) :: memory_op_result
   def update_memory(%Memory{} = memory, attrs) do
     memory
     |> Memory.changeset(attrs)
     |> Repo.update()
   end
 
+  @spec delete_memory(%Memory{}) :: {:ok, %Memory{}}
   def delete_memory(%Memory{} = memory) do
     Repo.delete(memory)
   end
